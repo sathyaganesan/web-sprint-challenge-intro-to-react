571474bbd7d7a161e46b7c132caae31b629e2e30
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2020-08-31 07:58:28 -0400

    Character file is done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+const [ricky, setRicky] = useState ([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+  useEffect(() => {
+    axios
+    .get(`https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/`)
+    .then((res) => {
+      setRicky(res.data.results);
+      // console.log(res.data);
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }, []);
+
+  // const newStarwar = starwar.results;
+  console.log(ricky);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {ricky.map((item) => {
+        return (<Character 
+          key = {item.id}
+          name = {item.name}
+          src = {item.image}
+          speices = {item.species}
+          gender = {item.gender}
+          status = {item.status}
+        />);
+      })} 
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from 'react';
+    const Character = (props) => {
+        return(
+            <div>
+               <h3>Character Name: {props.name} </h3>
+               <img src = {props.image} alt = "Ricky Morty"/>
+                <p>Speices: {props.speices}</p>
+                <p>Gender: {props.gender}</p>
+                <p>Status: {props.status}</p>
+            </div>
+        );
+    };
+export default Character;
